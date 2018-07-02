a0eec384980577ff55754aaa6ae2c9d843428ce0
yaninghuang <1036616721@qq.com>
2018-07-02 22:01:31 +0800

    public vue-cli-model

--- src/main.js
+++ src/main.js
@@ -13,8 +13,6 @@ import 'element-ui/lib/theme-chalk/index.css'
 import './assets/sass/iconfont.css'
 import './assets/sass/skin.scss'
 import './assets/sass/base.scss'
-import './assets/sass/login.scss'
-import './assets/sass/home.scss'
 
 Vue.use(ElementUI)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-const login = () => import('@/views/login')
-const home = () => import('@/views/home')
-const headTop = () => import('@/components/headTop')
-
 export default new Router({
-    
   mode: 'hash',
-  
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // },
     {
       path: '/',
-      name: 'login',
-      component: login
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: home,
-      children: [{
-        path: '/headTop',
-        component: headTop,
-        meta: ['公共头部分']
-      }]
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
